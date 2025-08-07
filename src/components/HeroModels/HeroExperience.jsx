b9d2ccfdd9df5d4245b8e90695cda39442aad75e
Reza Rizki A <rezariski.akbar@gmail.com>
2025-08-07 20:51:02 +0700

    selesai bikin 3d model hero dan juga button see my work onClick

--- src/components/HeroModels/HeroExperience.jsx
+++ src/components/HeroModels/HeroExperience.jsx
@@ -3,6 +3,8 @@ import { Canvas } from "@react-three/fiber";
 import { useMediaQuery } from "react-responsive";
 import { Room } from "./Room";
 import React from "react";
+import HeroLights from "./HeroLights";
+import Particles from "./Particles";
 
 const HeroExperience = () => {
   const isTablet = useMediaQuery({ query: "(max-width: 1024px)" });
@@ -10,9 +12,6 @@ const HeroExperience = () => {
 
   return (
     <Canvas camera={{ position: [0, 0, 15], fov: 45 }}>
-      <ambientLight intensity={0.2} color="#1a1a40" />
-      <directionalLight position={[5, 5, 5]} intensity={2} />
-
       <OrbitControls
         enablePan={false}
         enableZoom={!isTablet}
@@ -22,12 +21,15 @@ const HeroExperience = () => {
         maxPolarAngle={Math.PI / 2}
       />
 
+      <HeroLights />
+
       <group
-        scale={isMobile ? 0.7 : 1}
-        position={[0, -3.5, 0]}
+        scale={isMobile ? 0.9 : 1}
+        position={[0, -3, 0]}
         rotation={[0, -Math.PI / 4, 0]}
       >
         <Room />
+        <Particles />
       </group>
     </Canvas>
   );
